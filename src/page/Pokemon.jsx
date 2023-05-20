68f62f189409e74739064bfb6c22ce2ceb21b0cf
JuanSilva27 <juan.silva.0270@gmail.com>
2023-05-20 14:04:33 -0300

    agregan estilos para la pagina de cada Poke

--- src/page/Pokemon.jsx
+++ src/page/Pokemon.jsx
@@ -9,20 +9,39 @@ export const Pokemon = () => {
 
   useEffect(() => {
     getPokemonById(id);
-  },[id]);
-
+  }, [id]);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 bg-gray-100 max-w-2xl">
       {loading ? (
         <p>Loading...</p>
-      ) : pokemon  && pokemon.stats? (
-        <div className="bg-white rounded-lg shadow-lg p-4">
-          <h2 className="text-2xl font-bold mb-2">{pokemon.name}</h2>
-          <h3 className="text-lg font-medium mb-2">Stats:</h3>
-          <Stats stats={pokemon.stats}/>
+      ) : pokemon && pokemon.stats ? (
+        <div className="bg-white rounded-lg shadow-lg p-8">
+          <div className="flex items-center justify-center mb-4">
+            <img
+              src={pokemon.sprites.other.home.front_default}
+              alt="Pokemon Sprite"
+              className=" rounded-md h-44"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-gray-600">
+              <span className="font-bold">Weight:</span> {pokemon.weight}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-bold">Abilities:</span>{" "}
+              {pokemon.abilities
+                .map((ability) => ability.ability.name)
+                .join(", ")}
+            </p>
+          </div>
+          <h2 className="text-xl font-semibold mb-2 uppercase">{pokemon.name}</h2>
+          <h3 className="text-x1 font-semibold mb-2">Stats:</h3>
+          <Stats stats={pokemon.stats} />
         </div>
-      ):(<p>Loading...</p>)}
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
