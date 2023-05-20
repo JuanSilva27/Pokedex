c5ffed6046827946a9c1a4c6e8b659dcaad0446e
JuanSilva27 <juan.silva.0270@gmail.com>
2023-05-20 20:43:12 -0300

    se crea carpeta utils para las funciones que se puedan reusar en el projecto

--- src/page/Pokemon.jsx
+++ src/page/Pokemon.jsx
@@ -3,6 +3,7 @@ import usePokemon from "../hooks/usePokemon";
 import { useParams } from "react-router-dom";
 import { Stats } from "../components/Stats";
 import { Type } from "../components/Type";
+import { primerLetraMayus } from "../utils/primerLetraMayus";
 
 export const Pokemon = () => {
   const { id } = useParams();
@@ -25,25 +26,29 @@ export const Pokemon = () => {
               className=" rounded-md h-44 absolute"
             />
           </div>
-          <h1 className="text-center uppercase font-mono font-semibold text-[60px]">{pokemon.name}</h1>
+          <h1 className="text-center font-mono font-semibold text-[60px]">
+            {primerLetraMayus(pokemon.name)}
+          </h1>
           <div className="flex items-center justify-center">
             {pokemon.types.map((type) => (
               <Type key={type.type.name} {...type} />
             ))}
           </div>
-          <div className="mb-4 mt-4 pt-4">
+          <div className="mb-4 mt-4 pt-4 flex">
             <p className="text-gray-600">
               <span className="font-bold">Weight:</span> {pokemon.weight}
             </p>
             <p className="text-gray-600">
               <span className="font-bold">Abilities:</span>{" "}
               {pokemon.abilities
-                .map((ability) => ability.ability.name)
+                .map((ability) => primerLetraMayus(ability.ability.name))
                 .join(", ")}
             </p>
           </div>
-          <h3 className="text-x1 font-semibold mb-2">Stats:</h3>
-          <Stats stats={pokemon.stats} />
+          <div>
+            <h3 className="text-x1 font-semibold mb-2">Stats:</h3>
+            <Stats stats={pokemon.stats} />
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
