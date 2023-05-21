1cf5718fb7a53c7c13a682d60ff9c03951812997
JuanSilva27 <juan.silva.0270@gmail.com>
2023-05-21 19:57:55 -0300

    se mejoraron los estilos de la FilterBar

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -8,18 +8,18 @@ export const FilterBar = ({ onTypeSelection }) => {
 
   return (
     <div className="container flex flex-col items-center">
-      <span className="md:w-1/12 text-center">Tipos :</span>
+      <span className="md:w-1/12 text-center font-bold text-xl">Tipos :</span>
       <div className="w-11/12 flex flex-wrap">
         {arrayTypes.map((type) => (
           <div key={type} className="w-1/3 md:w-1/5 flex justify-around mb-4 mt-2">
-            <label htmlFor={type} className="w-1/2">{primerLetraMayus(type)}</label>
             <input
               type="checkbox"
               name={type}
               id={type}
               onChange={handleCheckBox}
-              className="w-1/2"
+              className="w-1/4"
             />
+            <label htmlFor={type} className="w-3/4 first-letter: font-semibold text-xl text-">{primerLetraMayus(type)}</label>
           </div>
         ))}
       </div>
