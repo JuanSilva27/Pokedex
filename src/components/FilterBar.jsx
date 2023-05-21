ca18fb509e1a6fabb23c0f40491e3c86484a8ba4
JuanSilva27 <juan.silva.0270@gmail.com>
2023-05-21 11:39:58 -0300

    se le dio una key unica a los div que son resultado de un mapeo

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -3,12 +3,13 @@ import { arrayTypes } from "../helpers/arrayTypes";
 import { primerLetraMayus } from "../helpers/primerLetraMayus";
 export const FilterBar = () => {
   return (
+    
     <div className="container">
       <div>
         <span>Tipo</span>
         {arrayTypes.map((type) => (
-          <div>
-            <input type="checkbox" name={type} id={type} />
+          <div key={type}>
+            <input type="checkbox" name={type} id={type}  />
             <label htmlFor={type}>{primerLetraMayus(type)}</label>
           </div>
         ))}
