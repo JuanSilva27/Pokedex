8881721ccdade51f7732fa7a51fed8b0a244ab0d
JuanSilva27 <juan.silva.0270@gmail.com>
2023-05-22 12:55:57 -0300

    se termina de pulir la pagina not Found

--- src/page/NotFoundPage.jsx
+++ src/page/NotFoundPage.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const NotFoundPage = () => {
   return (
-    <div className="bg-white p-5 shadow mt-10 rounded-md flex flex-wrap justify-center`">
+    <div className="bg-white p-5 shadow  rounded-md flex flex-wrap justify-center`">
       <div className="flex flex-col items-center justify-center">
-        <div className="flex w-[100%] ">
+        <div className="flex w-[100%] justify-center ">
           <span className="w-1/3 flex items-center justify-center text-[156px] font-bold">
             4
           </span>
-          <img className="w-1/3" src="/pokeball.png" alt="" />
+          <img className="w-1/4" src="/pokeball.png" alt="" />
           <span className="w-1/3 flex items-center justify-center text-[156px] font-bold">
             4
           </span>
@@ -19,7 +19,7 @@ export const NotFoundPage = () => {
             Pagina no encontrada
           </span>
           <Link to="/">
-            <button> Volver a la pagina principal</button>
+            <button className="bg-blue-600 m-5 p-5 rounded text-white font-semibold uppercase"> Volver a la pagina principal</button>
           </Link>
         </div>
       </div>
