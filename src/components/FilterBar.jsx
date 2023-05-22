b73afc509a8da5a5985468a772f277a4203219c0
JuanSilva27 <juan.silva.0270@gmail.com>
2023-05-21 21:22:40 -0300

    se agrego la barra de busqueda para filtrar por nombre o habilidad

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -8,6 +8,12 @@ export const FilterBar = ({ onTypeSelection }) => {
 
   return (
     <div className="container flex flex-col items-center">
+      <div className="m-4">
+        <form action="">
+          <input type="text" placeholder="Ingrese un el Nombre o Habilidad" className="w-64 h-12 rounded-l-2xl border-none" />
+          <button className="bg-blue-400 h-12 w-20 rounded-r-2xl" >Buscar</button>
+        </form>
+      </div>
       <span className="md:w-1/12 text-center font-bold text-xl">Tipos :</span>
       <div className="w-11/12 flex flex-wrap">
         {arrayTypes.map((type) => (
